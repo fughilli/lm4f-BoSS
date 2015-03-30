6757287cc744566c9611c3d613c1eeea3e28f574
fughilli <kbalke@ucla.edu>
2015-03-29 22:49:07 -0700

    Added stubs for pipe and file functions

--- file.c
+++ file.c
@@ -7,6 +7,8 @@
 
 #include "file.h"
 
+fd_assoc_t* file_table;
+
 const fd_funmap_t file_funmap =
 {
 		.close = file_close,
@@ -38,4 +40,24 @@ int32_t write(fd_t fd, uint8_t* buf, int32_t len)
 		return RW_INVALID;
 
 	return file_table[fd].funmap->write(fd, buf, len);
+}
+
+fd_t file_open(const char* fname, uint32_t mode, uint32_t flags)
+{
+	return FD_INVALID;
+}
+
+void file_close(fd_t fd)
+{
+
+}
+
+int32_t file_read(fd_t fd, uint8_t* buf, int32_t len)
+{
+	return RW_INVALID;
+}
+
+int32_t file_write(fd_t fd, uint8_t* buf, int32_t len)
+{
+	return RW_INVALID;
 }
--- pipe.c
+++ pipe.c
@@ -16,7 +16,7 @@ const fd_funmap_t pipe_funmap =
 
 fd_t pipe_create(size_t bufsiz)
 {
-
+	return FD_INVALID;
 }
 
 void pipe_close(fd_t fd)
@@ -26,11 +26,11 @@ void pipe_close(fd_t fd)
 
 int32_t pipe_read(fd_t fd, uint8_t* buf, int32_t len)
 {
-
+	return RW_INVALID;
 }
 
 int32_t pipe_write(fd_t fd, uint8_t* buf, int32_t len)
 {
-
+	return RW_INVALID;
 }
 
