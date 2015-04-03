096d840c5c9aa3112f529b9a7a88bf0e84c14058
fughilli <kbalke@ucla.edu>
2015-04-03 14:01:02 -0700

    Implemented file and gpio system call stubs

--- syscalls.h
+++ syscalls.h
@@ -10,6 +10,7 @@
 
 #include "thread.h"
 #include "syscall_numbers.h"
+#include "file.h"
 #include <stdint.h>
 
 //			"mov R0,#0\n\t"
@@ -143,20 +144,117 @@ inline uint32_t sys_fork()
 	return ret;
 }
 
-//inline void sys_set_port_dirs(uint32_t portbase, uint8_t dirs)
-//{
-//
-//}
-//
-//inline void sys_write_port(uint32_t portbase, uint8_t portmask, uint8_t levels)
-//{
-//
-//}
-//
-//inline uint8_t sys_read_port(uint32_t portbase)
-//{
-//
-//}
+inline void sys_set_port_dirs(uint32_t portbase, uint32_t dirs)
+{
+	asm volatile (
+			"mov R0,%0\n\t"
+			"mov R1,%1\n\t"
+			"mov R2,%2\n\t"
+			"svc $0x80"
+			: :
+			"r" (SYSCALL_SET_PORT_DIRS), "r" (portbase), "r" (dirs) : "memory", "0", "1", "2"
+	);
+}
+
+inline void sys_write_port(uint32_t portbase, uint32_t portmask, uint32_t levels)
+{
+	asm volatile (
+			"mov R0,%0\n\t"
+			"mov R1,%1\n\t"
+			"mov R2,%2\n\t"
+			"mov R3,%3\n\t"
+			"svc $0x80"
+			: :
+			"r" (SYSCALL_WRITE_PORT), "r" (portbase), "r" (portmask), "r" (levels) : "memory", "0", "1", "2", "3"
+	);
+}
+
+inline uint32_t sys_read_port(uint32_t portbase)
+{
+	uint32_t ret;
+	asm volatile (
+			"mov R0,%1\n\t"
+			"mov R1,%2\n\t"
+			"svc $0x80\n\t"
+			"mov %0,R0"
+			: "=r" (ret) : "r" (SYSCALL_READ_PORT), "r" (portbase) : "memory", "0", "1"
+	);
+	return ret;
+}
+
+inline void sys_close(fd_t fd)
+{
+	asm volatile (
+			"mov R0,%0\n\t"
+			"mov R1,%1\n\t"
+			"svc $0x80"
+			: : "r" (SYSCALL_CLOSE), "r" (fd) : "memory", "0", "1"
+	);
+}
+
+inline int32_t sys_read(fd_t fd, uint8_t* buf, int32_t len)
+{
+	int32_t ret;
+	asm volatile (
+			"mov R0,%1\n\t"
+			"mov R1,%2\n\t"
+			"mov R2,%3\n\t"
+			"mov R3,%4\n\t"
+			"svc $0x80\n\t"
+			"mov %0,R0"
+			: "=r" (ret) :
+			"r" (SYSCALL_READ), "r" (fd), "r" (buf), "r" (len) : "memory", "0", "1", "2", "3", "4"
+	);
+	return ret;
+}
+
+inline int32_t sys_write(fd_t fd, const uint8_t* buf, int32_t len)
+{
+	int32_t ret;
+	asm volatile (
+			"mov R0,%1\n\t"
+			"mov R1,%2\n\t"
+			"mov R2,%3\n\t"
+			"mov R3,%4\n\t"
+			"svc $0x80\n\t"
+			"mov %0,R0"
+			: "=r" (ret) :
+			"r" (SYSCALL_WRITE), "r" (fd), "r" (buf), "r" (len) : "memory", "0", "1", "2", "3", "4"
+	);
+	return ret;
+}
+
+inline int32_t sys_seek(fd_t fd, int32_t pos)
+{
+	int32_t ret;
+	asm volatile (
+			"mov R0,%1\n\t"
+			"mov R1,%2\n\t"
+			"mov R2,%3\n\t"
+			"svc $0x80\n\t"
+			"mov %0,R0"
+			: "=r" (ret) :
+			"r" (SYSCALL_SEEK), "r" (fd), "r" (pos) : "memory", "0", "1", "2", "3"
+	);
+	return ret;
+}
+
+inline uint32_t sys_ioctl(fd_t fd, uint32_t mask, void* arg)
+{
+	uint32_t ret;
+	asm volatile (
+			"mov R0,%1\n\t"
+			"mov R1,%2\n\t"
+			"mov R2,%3\n\t"
+			"mov R3,%4\n\t"
+			"svc $0x80\n\t"
+			"mov %0,R0"
+			: "=r" (ret) :
+			"r" (SYSCALL_IOCTL), "r" (fd), "r" (mask), "r" (arg) : "memory", "0", "1", "2", "3", "4"
+	);
+	return ret;
+}
+
 
 __attribute__((noreturn()))
 inline void sys_exit(int status)
