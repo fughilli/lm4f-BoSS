f638e5df96010b90a6eae162bbb257d029a81f5b
fughilli <kbalke@ucla.edu>
2015-04-03 13:20:51 -0700

    Added Serial_writebuf() and generalized to any UART module

--- debug_serial.c
+++ debug_serial.c
@@ -28,53 +28,124 @@
 //
 //void Serial_ISR();
 
-void Serial_init()
+const uint32_t uart_bases[8] =
+{
+UART0_BASE,
+UART1_BASE,
+UART2_BASE,
+UART3_BASE,
+UART4_BASE,
+UART5_BASE,
+UART6_BASE,
+UART7_BASE, };
+
+const uint32_t uart_peripherals[8] =
+{
+SYSCTL_PERIPH_UART0,
+SYSCTL_PERIPH_UART1,
+SYSCTL_PERIPH_UART2,
+SYSCTL_PERIPH_UART3,
+SYSCTL_PERIPH_UART4,
+SYSCTL_PERIPH_UART5,
+SYSCTL_PERIPH_UART6,
+SYSCTL_PERIPH_UART7, };
+
+const uint32_t gpio_peripherals[8] =
+{
+SYSCTL_PERIPH_GPIOA,
+SYSCTL_PERIPH_GPIOC,
+SYSCTL_PERIPH_GPIOD,
+SYSCTL_PERIPH_GPIOC,
+SYSCTL_PERIPH_GPIOC,
+SYSCTL_PERIPH_GPIOE,
+SYSCTL_PERIPH_GPIOD,
+SYSCTL_PERIPH_GPIOE };
+
+const uint32_t gpio_bases[8] =
+{
+GPIO_PORTA_BASE,
+GPIO_PORTC_BASE,
+GPIO_PORTD_BASE,
+GPIO_PORTC_BASE,
+GPIO_PORTC_BASE,
+GPIO_PORTE_BASE,
+GPIO_PORTD_BASE,
+GPIO_PORTE_BASE };
+
+const uint32_t gpio_configs[8] =
+{ GPIO_PA0_U0RX | GPIO_PA1_U0TX, GPIO_PC4_U1RX | GPIO_PC5_U1TX, GPIO_PD6_U2RX
+		| GPIO_PD7_U2TX, GPIO_PC6_U3RX | GPIO_PC7_U3TX, GPIO_PC4_U4RX
+		| GPIO_PC5_U4TX, GPIO_PE4_U5RX | GPIO_PE5_U5TX, GPIO_PD4_U6RX
+		| GPIO_PD5_U6TX, GPIO_PE0_U7RX | GPIO_PE1_U7TX };
+
+const uint32_t uart_gpio_pins[8] =
+{
+GPIO_PIN_0 | GPIO_PIN_1,
+GPIO_PIN_4 | GPIO_PIN_5,
+GPIO_PIN_6 | GPIO_PIN_7,
+GPIO_PIN_6 | GPIO_PIN_7,
+GPIO_PIN_4 | GPIO_PIN_5,
+GPIO_PIN_4 | GPIO_PIN_5,
+GPIO_PIN_4 | GPIO_PIN_5,
+GPIO_PIN_0 | GPIO_PIN_1 };
+
+void Serial_init(uint8_t module, uint32_t baud)
 {
 //	RXBuffer_head = RXBuffer_tail = 0;
 
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
-			UART_CONFIG_WLEN_8 | UART_CONFIG_PAR_NONE | UART_CONFIG_STOP_ONE);
-	GPIOPinConfigure(GPIO_PA0_U0RX | GPIO_PA1_U0TX);
-	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+	SysCtlPeripheralEnable(uart_peripherals[module]);
+	SysCtlPeripheralEnable(gpio_peripherals[module]);
+	UARTConfigSetExpClk(uart_bases[module], SysCtlClockGet(), baud,
+	UART_CONFIG_WLEN_8 | UART_CONFIG_PAR_NONE | UART_CONFIG_STOP_ONE);
+	GPIOPinConfigure(gpio_configs[module]);
+	GPIOPinTypeUART(gpio_bases[module], uart_gpio_pins[module]);
 
-	UARTIntDisable(UART0_BASE, 0xFFFFFFFF);
+	UARTIntDisable(uart_bases[module], 0xFFFFFFFF);
 
 //	UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8, UART_FIFO_RX1_8);
 //	UARTIntRegister(UART0_BASE, Serial_ISR);
 //	UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
 //	IntEnable(INT_UART0);
 
-	UARTEnable(UART0_BASE);
+	UARTEnable(uart_bases[module]);
 }
 
-void Serial_putc(char c)
+void Serial_putc(uint8_t module, char c)
 {
-	UARTCharPut(UART0_BASE, c);
+	UARTCharPut(uart_bases[module], c);
 }
 
-int Serial_getc()
+int Serial_getc(uint8_t module)
 {
-	if(!UARTCharsAvail(UART0_BASE))
+	if (!UARTCharsAvail(uart_bases[module]))
 		return -1;
-	return (int)UARTCharGet(UART0_BASE);
+	return (int) UARTCharGet(uart_bases[module]);
 }
 
-void Serial_puts(const char * s, uint16_t maxlen)
+void Serial_puts(uint8_t module, const char * s, uint16_t maxlen)
 {
 	uint16_t i;
-	for(i = 0; i < maxlen; i++)
+	for (i = 0; i < maxlen; i++)
 	{
-		if(s[i] == 0)
+		if (s[i] == 0)
 			break;
-		UARTCharPut(UART0_BASE, s[i]);
+		UARTCharPut(uart_bases[module], s[i]);
 	}
 }
 
-void Serial_flush()
+void Serial_writebuf(uint8_t module, const uint8_t* buf, uint32_t len)
 {
-	while(UARTBusy(UART0_BASE));
+	uint32_t i;
+	for (i = 0; i < len; i++)
+	{
+		UARTCharPut(uart_bases[module], buf[i]);
+	}
+}
+
+void Serial_flush(uint8_t module)
+{
+	while (UARTBusy(uart_bases[module]))
+		;
 }
 
 //uint8_t Serial_available()
--- debug_serial.h
+++ debug_serial.h
@@ -14,11 +14,23 @@
 extern "C" {
 #endif
 
-void Serial_init();
-void Serial_putc(char c);
-int Serial_getc();
-void Serial_puts(const char * s, uint16_t maxlen);
-void Serial_flush();
+#define UART0_MODULE (0)
+#define UART1_MODULE (1)
+#define UART2_MODULE (2)
+#define UART3_MODULE (3)
+#define UART4_MODULE (4)
+#define UART5_MODULE (5)
+#define UART6_MODULE (6)
+#define UART7_MODULE (7)
+
+#define UART_DEBUG_MODULE UART0_MODULE
+
+void Serial_init(uint8_t module, uint32_t baud);
+void Serial_putc(uint8_t module, char c);
+int Serial_getc(uint8_t module);
+void Serial_puts(uint8_t module, const char * s, uint16_t maxlen);
+void Serial_writebuf(uint8_t module, const uint8_t* buf, uint32_t len);
+void Serial_flush(uint8_t module);
 
 #ifdef __cplusplus
 }
