c16ee653925f4ee50b21163ab0419ed7185bedd6
fughilli <kbalke@ucla.edu>
2015-04-15 23:19:28 -0700

    Added OS configuration file

---
+++ os_config.h
@@ -0,0 +1,18 @@
+/*
+ * os_config.h
+ *
+ *  Created on: Apr 13, 2015
+ *      Author: Kevin
+ */
+
+#ifndef OS_CONFIG_H_
+#define OS_CONFIG_H_
+
+/*
+ * MEMORY_PROTECTION:
+ * 	0: No memory protection
+ * 	1: Processes limited to all flash and allocated RAM segment
+ */
+#define MEMORY_PROTECTION 0
+
+#endif /* OS_CONFIG_H_ */
