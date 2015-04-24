e755c3418a1ef3f5b0d61ed9cd9be28ad44ae147
fughilli <kbalke@ucla.edu>
2015-04-24 00:17:34 -0700

    Added macro for FLASH placement

---
+++ os_macros.h
@@ -0,0 +1,13 @@
+/*
+ * os_macros.h
+ *
+ *  Created on: Apr 23, 2015
+ *      Author: Kevin
+ */
+
+#ifndef OS_MACROS_H_
+#define OS_MACROS_H_
+
+#define ATTR_FLASH __attribute__((section("FLASH")))
+
+#endif /* OS_MACROS_H_ */
