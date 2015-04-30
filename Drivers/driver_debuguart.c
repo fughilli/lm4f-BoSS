d86d4179455b20c51576c8c0cead811007873919
fughilli <kbalke@ucla.edu>
2015-04-30 10:30:18 -0700

    Added debuguart driver

---
+++ Drivers/driver_debuguart.c
@@ -0,0 +1,54 @@
+/*
+ * driver_debuguart.c
+ *
+ *  Created on: Apr 30, 2015
+ *      Author: Kevin
+ */
+
+#include "driver_debuguart.h"
+#include "../debug_serial.h"
+
+void debuguart_init()
+{
+	Serial_init(UART_DEBUG_MODULE, 115200);
+}
+
+void debuguart_close(fd_t fd)
+{
+	;
+}
+
+int32_t debuguart_read(fd_t fd, uint8_t* buf, int32_t len)
+{
+	int32_t i = 0;
+	while(i < len && Serial_avail(UART_DEBUG_MODULE))
+	{
+		buf[i] = Serial_getc(UART_DEBUG_MODULE);
+		i++;
+	}
+	return i;
+}
+
+int32_t debuguart_write(fd_t fd, const uint8_t* buf, int32_t len)
+{
+	Serial_writebuf(UART_DEBUG_MODULE, buf, len);
+}
+
+int32_t debuguart_seek(fd_t fd, int32_t pos)
+{
+	return SEEK_INVALID;
+}
+
+uint32_t debuguart_ioctl(fd_t fd, uint32_t mask, void* arg)
+{
+	return IOCTL_INVALID;
+}
+
+const fd_funmap_t debuguart_funmap =
+{
+	.close = debuguart_close,
+	.ioctl = NONE,
+	.seek = NONE,
+	.read = debuguart_read,
+	.write = debuguart_write
+};
---
+++ Drivers/driver_debuguart.h
@@ -0,0 +1,17 @@
+/*
+ * driver_debuguart.h
+ *
+ *  Created on: Apr 30, 2015
+ *      Author: Kevin
+ */
+
+#ifndef DRIVER_DEBUGUART_H_
+#define DRIVER_DEBUGUART_H_
+
+#include "../file.h"
+
+extern const fd_funmap_t debuguart_funmap;
+
+void debuguart_init();
+
+#endif /* DRIVER_DEBUGUART_H_ */
