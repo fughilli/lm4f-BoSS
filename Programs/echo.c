e2ef486f2df507cd19cecd4fb423b93adcb38fcc
fughilli <kbalke@ucla.edu>
2015-04-30 09:28:20 -0700

    Moved echo to its own Program file

---
+++ Programs/echo.c
@@ -0,0 +1,101 @@
+/*
+ * echo.c
+ *
+ *  Created on: Apr 28, 2015
+ *      Author: Kevin
+ */
+
+#include "echo.h"
+
+int echo_main(char* argv[], int argc)
+{
+	int i = 1;
+	bool escaped = false;
+	bool nonewline = false;
+	if (argc > 1)
+	{
+		while(argv[i][0] == '-' && i < argc)
+		{
+			int arglen;
+			if((arglen = fast_strlen(argv[i])) > 1)
+			{
+				while(--arglen)
+				{
+					switch(argv[i][arglen])
+					{
+					case 'n':
+						nonewline = true;
+						break;
+					case 'e':
+						escaped = true;
+						break;
+					}
+				}
+			}
+			i++;
+		}
+
+		while (i < argc)
+		{
+			if(escaped)
+			{
+				char curchr, placechr;
+				bool escapeseq = false;
+				while((curchr = *(argv[i])))
+				{
+					if(curchr == '\\')
+					{
+						if(escapeseq)
+						{
+							placechr = '\\';
+							escapeseq = false;
+						}
+						else
+						{
+							escapeseq = true;
+						}
+					}
+					else
+					{
+						if(escapeseq)
+						{
+							escapeseq = false;
+							switch(curchr)
+							{
+							case 'n':
+								placechr = '\n';
+								break;
+							case 't':
+								placechr = '\t';
+								break;
+							case 'r':
+								placechr = '\r';
+								break;
+							case 'a':
+								placechr = '\a';
+								break;
+							default:
+								placechr = curchr;
+								break;
+							}
+						}
+						else
+						{
+							placechr = curchr;
+						}
+						sys_putc(placechr);
+					}
+					argv[i]++;
+				}
+			}
+			else
+				sys_puts(argv[i], fast_strlen(argv[i]));
+			i++;
+			if(i < argc)
+				sys_putc(' ');
+		}
+	}
+	if(!nonewline)
+		sys_puts("\r\n", 2);
+    return 0;
+}
---
+++ Programs/echo.h
@@ -0,0 +1,15 @@
+/*
+ * echo.h
+ *
+ *  Created on: Apr 28, 2015
+ *      Author: Kevin
+ */
+
+#ifndef ECHO_H_
+#define ECHO_H_
+
+#include "osprogram.h"
+
+int echo_main(char* argv[], int argc);
+
+#endif /* ECHO_H_ */
